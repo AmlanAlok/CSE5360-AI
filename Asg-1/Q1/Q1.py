89d4fba0575e80e735a621911abd38aeccf3b515
Amlan Alok <amlanalok@gmail.com>
2022-10-22 12:16:34 -0500

    Q1: reading command line input arguments

---
+++ Asg-1/Q1/Q1.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    print('Q1')
+    print(sys.argv)
+
+    if len(sys.argv) == 4:
+        '''Uninformed Search'''
+
+    elif len(sys.argv) == 5:
+        '''Informed Search'''
+
+
+
+if __name__ == "__main__":
+    main()
+
+#   python3 Q1.py input1.txt Bremen Kassel
