359de68603052127f9c9f884648aaf2facc81208
Amlan Alok <amlanalok@gmail.com>
2022-10-23 16:31:52 -0500

    Q2: Random move

--- Asg-1/Q2/Q2.py
+++ Asg-1/Q2/Q2.py
@@ -33,7 +33,7 @@ def human_player_turn():
     return col_idx
 
 
-def computer_player_turn():
+def computer_player_turn_random():
     col = randint(1, 7)
     if 1 <= col <= 7:
         col_idx = col - 1
@@ -71,7 +71,7 @@ def update_board(board, game_state, player, chosen_col_idx, score_dict):
             human_col = human_player_turn()
             return update_board(board, game_state, 'Human', human_col, score_dict)
         elif player == 'Computer':
-            computer_col = computer_player_turn()
+            computer_col = computer_player_turn_random()
             return update_board(board, game_state, 'Computer', computer_col, score_dict)
 
     return board, game_state, score_dict
@@ -199,9 +199,16 @@ def main():
         human_col = human_player_turn()
         board, game_state, score_dict = update_board(board, game_state, HUMAN, human_col, score_dict)
         # display_game(board)
-        computer_col = computer_player_turn()
+        computer_col = computer_player_turn_random()
         board, game_state, score_dict = update_board(board, game_state, COMPUTER, computer_col, score_dict)
         display_game(board, score_dict)
+
+    if score_dict[HUMAN] > score_dict[COMPUTER]:
+        print('YOU WIN')
+    elif score_dict[HUMAN] < score_dict[COMPUTER]:
+        print('YOU LOSE')
+    else:
+        print('DRAW')
     print('END')
 
 
