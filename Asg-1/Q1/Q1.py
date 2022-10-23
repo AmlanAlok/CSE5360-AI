5a8b89dbc1ebf6707d5d9dcb98c5c0ae75891bdb
Amlan Alok <amlanalok@gmail.com>
2022-10-23 03:47:23 -0500

    Q1: A* search added

--- Asg-1/Q1/Q1.py
+++ Asg-1/Q1/Q1.py
@@ -4,6 +4,7 @@ from collections import deque
 TO = 'to'
 FROM = 'from'
 DISTANCE = 'distance'
+FN = 'fn'
 
 
 def clean_data(line):
@@ -27,6 +28,9 @@ def organise_map(input_data):
 
     for record in input_data:
         first_city, second_city, distance = record[0], record[1], float(record[2])
+        ''' To avoid edge case scenario which might cause infinite loop'''
+        if distance == 0:
+            distance = 0.01
 
         if first_city in d:
             d[first_city].append([second_city, distance, first_city])
@@ -77,7 +81,7 @@ def uniform_cost_search(origin, destination, map_dict):
                    DISTANCE: 0.0})
 
     visited, route_traversed, expanded_arr = [], [], []
-    popped, generated, expanded = 0, 0, 0
+    popped, generated, expanded = 0, 1, 0
 
     while len(fringe) > 0:
         fringe_element = fringe.popleft()
@@ -112,6 +116,71 @@ def uniform_cost_search(origin, destination, map_dict):
     print('Route:\nNone')
 
 
+def organise_heuristics(input_data):
+    d = {}
+
+    for record in input_data:
+        city, distance = record[0], float(record[1])
+        d[city] = distance
+    return d
+
+
+def add_successors_to_a_star_fringe(fringe, successors, heuristics_dict):
+    def key_col_value(t):
+        return t[FN]
+
+    for arr in successors:
+        from_city, to_city, dist = arr[2], arr[0], arr[1]
+        fringe.append({FROM: from_city,
+                       TO: to_city,
+                       DISTANCE: dist,
+                       FN: dist + heuristics_dict[to_city]})
+
+    return deque(sorted(fringe, key=key_col_value))
+
+
+def a_start_search(origin, destination, map_dict, heuristics_dict):
+    fringe = deque()
+    fringe.append({FROM: '',
+                   TO: origin,
+                   DISTANCE: 0.0})
+
+    visited, route_traversed, expanded_arr = [], [], []
+    popped, generated, expanded = 0, 1, 0
+
+    while len(fringe) > 0:
+        fringe_element = fringe.popleft()
+        current_location = fringe_element[TO]
+        popped += 1
+
+        if current_location not in visited:
+            route_traversed.append(fringe_element)
+            visited.append(current_location)
+
+            if current_location == destination:
+                route_path, total_distance = route_tracker(route_traversed, origin, fringe_element)
+                print('Nodes Popped:', popped)
+                print('Nodes Expanded:', expanded)
+                print('Nodes Generated:', generated)
+                print('Distance:', total_distance, ' km')
+                for i in range(len(route_path) - 1, -1, -1):
+                    arr = route_path[i]
+                    print(arr[FROM], 'to', arr[TO], ',', arr[DISTANCE], 'km')
+                return
+
+            successors = map_dict[current_location]
+            generated += len(successors)
+            fringe = add_successors_to_a_star_fringe(fringe, successors, heuristics_dict)
+            expanded += 1
+            expanded_arr.append(current_location)
+
+    print('Nodes Popped:', popped)
+    print('Nodes Expanded:', expanded)
+    print('Nodes Generated:', generated)
+    print('Distance: infinity')
+    print('Route:\nNone')
+
+
 def main():
     print('Q1')
     # print(sys.argv)
@@ -121,16 +190,22 @@ def main():
     # destination = sys.argv[3]
 
     input_filename = 'input1.txt'
-    # origin = 'Bremen'
-    # destination = 'Kassel'
-    origin = 'London'
+
+    origin = 'Bremen'
     destination = 'Kassel'
+    # origin = 'London'
+    # destination = 'Kassel'
 
     input_data = read_input(input_filename)
-
     map_dict = organise_map(input_data)
 
     uniform_cost_search(origin, destination, map_dict)
+
+    heuristics_filename = 'h_kassel.txt'
+    heuristics_data = read_input(heuristics_filename)
+
+    heuristics_dict = organise_heuristics(heuristics_data)
+    a_start_search(origin, destination, map_dict, heuristics_dict)
 
     # if len(sys.argv) == 4:
     #     '''Uninformed Search'''
@@ -138,6 +213,7 @@ def main():
     #
     # elif len(sys.argv) == 5:
     #     '''Informed Search'''
+    print('END')
 
 
 if __name__ == "__main__":
