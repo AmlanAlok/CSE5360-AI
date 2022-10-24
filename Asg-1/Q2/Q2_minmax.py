0af44a958235f1887abcb0640b59af087847f709
Amlan Alok <amlanalok@gmail.com>
2022-10-24 01:38:19 -0500

    Q2: one_move_mode added

--- Asg-1/Q2/Q2_minmax.py
+++ Asg-1/Q2/Q2_minmax.py
@@ -1,7 +1,8 @@
 from random import randint
 import sys, os
 import math as m
-from game_rules import *
+# from game_rules import *
+from game_rules_12 import *
 
 HUMAN = 'Human'
 COMPUTER = 'Computer'
@@ -23,11 +24,13 @@ def get_fresh_board(rows, columns):
 
 
 def display_game(game, score_dict):
-    print('--------------------\n')
+    # print('--------------------\n')
     for arr in reversed(game):
         print(arr)
+    print('============================')
     print(score_dict)
-    print('\n--------------------\n')
+    print('============================')
+    # print('\n--------------------\n')
 
 
 def human_player_turn():
@@ -92,15 +95,24 @@ def computer_player_turn(board, game_state, token_dict):
     for col_idx in range(cols):
         score = position_score(board, game_state, col_idx, max_row_idx, token_dict)
         pos_score_arr[col_idx] = score
-    print('final')
+
     max_score = max(pos_score_arr)
     # game_state['score'] += max_score
+
+    all_zero = True
+
+    for i in range(len(pos_score_arr)):
+        if pos_score_arr[i] != 0:
+            all_zero = False
+
     put_pos_idx = pos_score_arr.index(max_score)
+
+    if all_zero:
+        return computer_player_turn_random()
     return put_pos_idx
 
 
-def main():
-    print('Q2 --')
+def interactive_mode():
     board, game_state = get_fresh_board(6, 7)
     print('New Game')
     score_dict = init_scores()
@@ -122,6 +134,62 @@ def main():
     else:
         print('DRAW')
     print('END')
+
+
+def clean_data(line):
+    return list(line.replace('\n', ''))
+
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        input_data = f.readlines()
+
+        ''' Last line says who plays next '''
+        next_player_token = int(input_data.pop())
+
+        clean_input = list(map(clean_data, input_data))
+        f.close()
+    return clean_input, next_player_token
+
+
+def create_game_board(rows, columns, input_data):
+    game_board = [[0] * columns for i in range(rows)]
+    game_state = {}
+    total_zeros = 0
+
+    for col_idx in range(7):
+        game_state[col_idx] = 0
+
+    for c in range(columns):
+        s = 0
+        for r in range(rows-1, -1, -1):
+            data_point = int(input_data[r][c])
+            if data_point != 0:
+                game_board[rows-1-r][c] = data_point
+                s += 1
+            else:
+                total_zeros += 1
+        game_state[c] = s
+
+    game_state['holes_left'] = total_zeros
+    game_state['score'] = 0
+
+    return game_board, game_state
+
+
+def one_move_mode():
+    filename = 'input2.txt'
+    input_data, next_player_token = read_input(filename)
+    board, game_state = create_game_board(6, 7, input_data)
+    score_dict = init_scores()
+    display_game(board, score_dict)
+    print('END')
+
+
+def main():
+    print('Q2 --')
+    one_move_mode()
+    # interactive_mode()
 
 
 if __name__ == "__main__":
