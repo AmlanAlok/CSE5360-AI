c481cd0e7d7be3ab97e054aa2b9b15aa5a735048
Amlan Alok <amlanalok@gmail.com>
2022-10-25 10:17:13 -0500

    Q2: final submission files

---
+++ Asg-1/Task2/depth.sh
@@ -0,0 +1,17 @@
+
+
+time python3 maxconnect4.py one-move input1.txt output4.txt 2
+time python3 maxconnect4.py one-move input1.txt output4.txt 3
+time python3 maxconnect4.py one-move input1.txt output4.txt 4
+time python3 maxconnect4.py one-move input1.txt output4.txt 5
+time python3 maxconnect4.py one-move input1.txt output4.txt 6
+time python3 maxconnect4.py one-move input1.txt output4.txt 7
+time python3 maxconnect4.py one-move input1.txt output4.txt 8
+time python3 maxconnect4.py one-move input1.txt output4.txt 9
+time python3 maxconnect4.py one-move input1.txt output4.txt 10
+time python3 maxconnect4.py one-move input1.txt output4.txt 11
+time python3 maxconnect4.py one-move input1.txt output4.txt 12
+time python3 maxconnect4.py one-move input1.txt output4.txt 13
+time python3 maxconnect4.py one-move input1.txt output4.txt 14
+time python3 maxconnect4.py one-move input1.txt output4.txt 15
+
