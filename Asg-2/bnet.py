43418bb91898aaee2130dc83077a1f3cc58b6e02
Amlan Alok <amlanalok@gmail.com>
2022-12-13 17:25:24 -0600

    starting Assignment 2

---
+++ Asg-2/bnet.py
@@ -0,0 +1,18 @@
+
+
+class BayesianNetwork:
+    def __init__(self, burglary, earthquake, alarm, john_calls, mary_calls):
+        self.burglary = burglary
+        self.earthquake = earthquake
+        self.alarm = alarm
+        self.john_calls = john_calls
+        self.mary_calls = mary_calls
+
+
+
+
+def main():
+    print('Hi')
+
+if __name__ == '__main__':
+    main()
